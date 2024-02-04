10999994c079b97fdcf49f92c4d8a8214b684f0e
KieraBacon <55027850+KieraBacon@users.noreply.github.com>
2024-02-04 02:16:09 -0500

    err

---
+++ Assets/Project/Scripts/Clickables/ProjectTextureOnSurface.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Clickables
+{
+    public class ProjectTextureOnSurface : MonoBehaviour
+    {
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private float _scale;
+        private Vector2Int pos;
+
+        public void Update()
+        {
+            pos = new Vector2Int(Random.Range(0, _sprite.texture.width), Random.Range(0, _sprite.texture.height));
+            Vector3 vectorPos = new Vector3(pos.x, 1, pos.y) * _scale;
+            Color c = _sprite.texture.GetPixel(pos.x, pos.y);
+            Debug.DrawRay(vectorPos, Vector3.up * 0.1f, c, 100);
+        }
+    }
+}
